e3755354afa249d39617c9bae23f784bfcb522c6
codyt <codyt@VIV>
2024-08-30 10:28:34 -0400

    Add project files.

---
+++ Chapter2Ex2_Cody_Drumheller.cpp
@@ -0,0 +1,34 @@
+/*
+Title: Chapter 2 Exercise 2 - Do something
+File Name: Chapter2Ex2_Cody_Drumheller.cpp
+Programmer: Cody Drumheller
+Date: 20240830
+Requirements: Class Walkthrough
+
+
+
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number1;
+    int number2;
+    int answer;
+    cout << "please enter a number\n";
+    cin >> number1;
+    cout << "please enter a number\n";
+    cin >> number2;
+    answer = number1 + number2;
+    cout << "The sum of " << number1 << " and " << number2 << " is " << answer;
+
+
+
+    return 0;// This is to return the value defined by...
+
+
+}
+
